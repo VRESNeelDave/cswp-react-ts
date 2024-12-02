e9809bf7fcd5731017f283291264580326da0c50
QUTNeelDave <n11000449@qut.edu.au>
2024-12-02 17:28:11 +1000

    bug fix

--- src/components/Appbar/Appbar.tsx
+++ src/components/Appbar/Appbar.tsx
@@ -1,4 +1,4 @@
-import React, { Children } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
